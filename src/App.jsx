6a03ddd1a21760b507d45e53c1d17de5fc0385a2
Saksham Khare <sakshamkhare19@gmail.com>
2024-06-21 13:31:46 +0530

    kind of memo code

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,16 @@ function App() {
   // }, []);
 
   // todo id code
-  const [buttonId, setButtonId] = useState(1);
+  // const [buttonId, setButtonId] = useState(1);
+
+  // Memo
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+
+  let count = 0;
+  for (let i = 1; i <= inputValue; i++) {
+    count = count + i;
+  }
 
   return (
     // <>
@@ -40,48 +49,70 @@ function App() {
     //     <Counter key={index} item={item} removeItem={removeItem} />
     //   ))}
     // </>
-    <>
-      {/* state hook code */}
-      {/* <State /> */}
-      {/* code of effect hook to render all todos not infinitely. */}
-      {/* {effect && effect.length > 0 ? (
-        effect.map((e) => (
-          <Effect key={e.id} title={e.title} description={e.description} />
-        ))
-      ) : (
-        <p>No data available</p>
-      )} */}
+    // <>
+    //   {/* state hook code */}
+    //   {/* <State /> */}
+    //   {/* code of effect hook to render all todos not infinitely. */}
+    //   {/* {effect && effect.length > 0 ? (
+    //     effect.map((e) => (
+    //       <Effect key={e.id} title={e.title} description={e.description} />
+    //     ))
+    //   ) : (
+    //     <p>No data available</p>
+    //   )} */}
 
-      {/* todo id code */}
+    //   {/* todo id code */}
+    //   <button
+    //     onClick={() => {
+    //       setButtonId(1);
+    //     }}
+    //   >
+    //     1
+    //   </button>
+    //   <button
+    //     onClick={() => {
+    //       setButtonId(2);
+    //     }}
+    //   >
+    //     2
+    //   </button>
+    //   <button
+    //     onClick={() => {
+    //       setButtonId(3);
+    //     }}
+    //   >
+    //     3
+    //   </button>
+    //   <button
+    //     onClick={() => {
+    //       setButtonId(4);
+    //     }}
+    //   >
+    //     4
+    //   </button>
+    //   <Todo id={buttonId} />
+    // </>
+
+    // useMemo
+    <>
+      <input
+        type="text"
+        placeholder="Find sum from 1 to n"
+        required
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+      <h1>
+        Sum from 1 to {inputValue} is {count}
+      </h1>
       <button
         onClick={() => {
-          setButtonId(1);
+          setCounter(counter + 1);
         }}
       >
-        1
+        Counter ({counter})
       </button>
-      <button
-        onClick={() => {
-          setButtonId(2);
-        }}
-      >
-        2
-      </button>
-      <button
-        onClick={() => {
-          setButtonId(3);
-        }}
-      >
-        3
-      </button>
-      <button
-        onClick={() => {
-          setButtonId(4);
-        }}
-      >
-        4
-      </button>
-      <Todo id={buttonId} />
     </>
   );
 }
